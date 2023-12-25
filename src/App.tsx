a8b238cfa658b5f63aa991c0a06d090e7f64fffc
Devansh-pathak <69319667+Devansh-pathak@users.noreply.github.com>
2023-12-25 21:15:18 +0530

    Commit 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { WishlistProvider } from './hooks/useWishlist';
 import HomePage from './components/HomePage';
-import ProductList from './components/ProductList';
+
 
 const App = () => {
   return (
